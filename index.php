7da1a27ad745ae673d7e8ca041ce46534e082547
Michael Kevin Victor <me@mikevictor.me>
2014-10-08 23:27:11 +0200

    Organized Index

--- index.php
+++ index.php
@@ -1,101 +1,11 @@
 <?php
 require 'header.php';
-
-//Get Parameters
-if(isset($_GET['committee']) && !empty($_GET['committee'])){
-	$committee = $_GET['committee'];
-} else {
-	$committee = 'unset';
-}
-
-if(isset($_GET['search']) && !empty($_GET['search'])){
-	$search = $_GET['search'];
-} else {
-	$search = 'unset';
-}
-
-if(isset($_GET['page']) && !empty($_GET['page'])){
-	$page = $_GET['page'];
-	$offset = $page * 20;
-} else {
-	$page = 'unset';
-	$offset = 0;
-}
-
-if($page == 'unset'){
-	$previous_page = 0;
-	$next_page = 2;
-} else {
-	$previous_page = $page - 1;
-	$next_page = $page + 1;
-}
-
-$where_query = "";
-//Process Parameters
-if($committee != 'unset'){
-	$where_query .= " AND `committee_id`=$committee";
-}
-
-if($search != 'unset'){
-	$where_query .= " AND `content_title` LIKE '%$search%'";
-}
-
-$next_page_url = http_build_query(array('committee'=>$committee, 'search'=>$search, 'page'=>$next_page));
-$previous_page_url = http_build_query(array('committee'=>$committee, 'search'=>$search, 'page'=>$previous_page));
-
 ?>
 <div class="container">
-	<br>
-	<div class="row">
-		<div class="col-md-8">
-			<form class="form-inline" role="form">
-			  <div class="form-group">
-			    <input type="text" class="form-control" placeholder="Enter Terms" id="search_bar" size="100" name="search" value="<?php if($search != 'unset') {echo $search;}  ?>">
-			  </div>
-			  <button type="submit" class="btn btn-default">Search</button>
-			</form>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-2">
-			<span class="text-left"><a href="index.php?<?php echo $previous_page_url; ?>">Previous Page</a></span>
-		</div>
-		<div class="col-md-2 col-md-offset-8 text-right">
-			<span class="text-right"><a href="index.php?<?php echo $next_page_url; ?>">Next Page</a></span>
-		</div>
-		
-		
-	</div>
-	<table class="table table-striped">
-		<tr>
-			<td>Wiki Name</td>
-			<td>Committee</td>
-			<td>Date</td>
-		</tr>
-<?php
-$query = "SELECT * from `basic` WHERE 1 ".$where_query." ORDER BY `last_updated_date` DESC LIMIT $offset, 20";
-$result = mysql_query($query);
-
-while($row = mysql_fetch_assoc($result)){
-	echo '<tr>';
-	echo '<td><a href="view_wiki.php?id='.$row['content_id'].'">'.$row['content_title'].'</a></td>';
-	echo '<td><a href="index.php?committee='.$row['committee_id'].'">'.get_committee($row['committee_id']).'</a></td>';
-	echo '<td>'.$row['last_updated_date_display'].'</td>';
-	echo '</tr>';
-}
-?>
-	</table>
-		<div class="row">
-		<div class="col-md-2">
-			<span class="text-left"><a href="index.php?<?php echo $previous_page_url; ?>">Previous Page</a></span>
-		</div>
-		<div class="col-md-2 col-md-offset-8 text-right">
-			<span class="text-right"><a href="index.php?<?php echo $next_page_url; ?>">Next Page</a></span>
-		</div>
-		
-		
-	</div>
+	<h2 class="text-center">Welcome to the myaiesec.net Wiki Archives</h2>
+	<h4 class="text-center"><a href="list.php">Access them here >></a></h4>
 </div>
+
 <?php
 require 'footer.php';
 ?>
--- list.php
+++ list.php
@@ -22,6 +22,14 @@ if(isset($_GET['page']) && !empty($_GET['page'])){
 	$offset = 0;
 }
 
+if($page == 'unset'){
+	$previous_page = 0;
+	$next_page = 2;
+} else {
+	$previous_page = $page - 1;
+	$next_page = $page + 1;
+}
+
 $where_query = "";
 //Process Parameters
 if($committee != 'unset'){
@@ -32,6 +40,9 @@ if($search != 'unset'){
 	$where_query .= " AND `content_title` LIKE '%$search%'";
 }
 
+$next_page_url = http_build_query(array('committee'=>$committee, 'search'=>$search, 'page'=>$next_page));
+$previous_page_url = http_build_query(array('committee'=>$committee, 'search'=>$search, 'page'=>$previous_page));
+
 ?>
 <div class="container">
 	<br>
@@ -40,8 +51,6 @@ if($search != 'unset'){
 			<form class="form-inline" role="form">
 			  <div class="form-group">
 			    <input type="text" class="form-control" placeholder="Enter Terms" id="search_bar" size="100" name="search" value="<?php if($search != 'unset') {echo $search;}  ?>">
-			  	<input type="hidden" value="<?php if($committee != 'unset') {echo $committee;} ?>">
-			  	<input type="hidden" value="<?php if($page != 'unset') {echo $page;} ?>">
 			  </div>
 			  <button type="submit" class="btn btn-default">Search</button>
 			</form>
@@ -49,10 +58,10 @@ if($search != 'unset'){
 	</div>
 	<div class="row">
 		<div class="col-md-2">
-			<span class="text-left">Previous Page</span>
+			<span class="text-left"><a href="list.php?<?php echo $previous_page_url; ?>">Previous Page</a></span>
 		</div>
 		<div class="col-md-2 col-md-offset-8 text-right">
-			<span class="text-right">Next Page</span>
+			<span class="text-right"><a href="list.php?<?php echo $next_page_url; ?>">Next Page</a></span>
 		</div>
 		
 		
@@ -64,24 +73,28 @@ if($search != 'unset'){
 			<td>Date</td>
 		</tr>
 <?php
-echo $query = "SELECT * from `basic` WHERE 1 ".$where_query." ORDER BY `last_updated_date` DESC LIMIT $offset, 20";
+$query = "SELECT * from `basic` WHERE 1 ".$where_query." ORDER BY `last_updated_date` DESC LIMIT $offset, 20";
 $result = mysql_query($query);
-
-while($row = mysql_fetch_assoc($result)){
-	echo '<tr>';
-	echo '<td><a href="view_wiki.php?id='.$row['content_id'].'">'.$row['content_title'].'</a></td>';
-	echo '<td><a href="index.php?committee='.$row['committee_id'].'">'.get_committee($row['committee_id']).'</a></td>';
-	echo '<td>'.$row['last_updated_date_display'].'</td>';
-	echo '</tr>';
+$no_of_rows = mysql_num_rows($result);
+if($no_of_rows!=0){
+	while($row = mysql_fetch_assoc($result)){
+		echo '<tr>';
+		echo '<td><a href="view_wiki.php?id='.$row['content_id'].'">'.$row['content_title'].'</a></td>';
+		echo '<td><a href="list.php?committee='.$row['committee_id'].'">'.get_committee($row['committee_id']).'</a></td>';
+		echo '<td>'.$row['last_updated_date_display'].'</td>';
+		echo '</tr>';
+	}
+} else  {
+	echo '<h2>Unfortunately, your search parameters did not report any results.</h2>';
 }
 ?>
 	</table>
 		<div class="row">
 		<div class="col-md-2">
-			<span class="text-left">Previous Page</span>
+			<span class="text-left"><a href="list.php?<?php echo $previous_page_url; ?>">Previous Page</a></span>
 		</div>
 		<div class="col-md-2 col-md-offset-8 text-right">
-			<span class="text-right">Next Page</span>
+			<span class="text-right"><a href="list.php?<?php echo $next_page_url; ?>">Next Page</a></span>
 		</div>
 		
 		
